d515103c98cffdb7ec51138d3a57b27ab95e92d4
Camilla <milla_mendes83@hotmail.com>
2025-01-20 16:28:50 -0300

    Learning DDD with TS

--- DDD/README.md
+++ DDD/README.md
@@ -3,3 +3,11 @@
 ### Value Object
 
 Value objects represent an item in the application's domain that does not have its own identity but is defined exclusively by its attributes. This means that two value objects are considered equal if their attributes are the same, regardless of whether they are different instances. They are immutable and are not supposed to be changed after they are created. To change a value object, it is necessary to create a new instance of the object.
+
+### Entity
+
+Entities are objects in the application's domain that have a unique identity and are distinguished from other objects by that identity, rather than by their attributes. An entity's identity is usually represented by a unique identifier, such as a primary key. Unlike value objects, entities are mutable, meaning their attributes can change over time as long as their identity remains the same. They are crucial for modeling the business rules and processes that rely on unique, identifiable items.
+
+### Aggregate
+
+Aggregates are clusters of related entities and value objects that are treated as a single unit for the purpose of data changes. Each aggregate has a root entity, known as the Aggregate Root, which enforces the consistency of changes within the aggregate. External objects or processes should only interact with the aggregate through its root. Aggregates help to define boundaries in the domain model and ensure that business rules and invariants are consistently applied.
--- DDD/index.ts
+++ DDD/index.ts
@@ -1,13 +1,3 @@
-class Item {
-  constructor(
-    readonly id: string,
-    public name: string,
-    public price: number,
-    public sku: string,
-    public quantity: number,
-  ) { }
-}
-
 class Address {
   constructor(
     readonly city: string,
@@ -19,7 +9,11 @@ class Address {
 
 class Zip {
   constructor(
-    readonly value: string) { }
+    readonly value: string) {
+    if (!/^\d{8}$/.test(value)) {
+      throw new Error("Invalid zip code");
+    }
+  }
 
   // Format to correct format to zip code
   format() {
@@ -27,29 +21,141 @@ class Zip {
   }
 }
 
+class Price {
+  private cents: number;
+
+  constructor(value: number) {
+    this.cents = Math.round(value * 100);
+  }
+
+  get value() {
+    return this.cents / 100;
+  }
+
+  add(amount: Price) {
+    return new Price(this.value + amount.value);
+  }
+
+  sub(amount: Price) {
+    return new Price(this.value - amount.value);
+  }
+
+  multiply(value: number) {
+    return new Price(this.value * value);
+  }
+
+  divide(value: number) {
+    return new Price(this.value / value);
+  }
+
+  discount(value: number) {
+    return new Price(this.value * (1 - value));
+  }
+}
+
+class Item {
+  constructor(
+    readonly id: Id,
+    public name: string,
+    public price: Price,
+    public sku: string,
+    public quantity: number,
+  ) {
+    if (name === undefined) {
+      throw new Error("Name is required");
+    }
+
+    if (price === undefined) {
+      throw new Error("Price is required");
+    }
+
+    if (sku === undefined) {
+      throw new Error("Sku is required");
+    }
+
+    if (price.value < 0) {
+      throw new Error("Price must be positive");
+    }
+
+    if (quantity < 0) {
+      throw new Error("Quantity must be positive");
+    }
+  }
+
+  getTotal() {
+    return this.price.multiply(this.quantity);
+  }
+}
+
+
+class Id {
+  constructor(readonly value: string) {
+    if (value === undefined) {
+      throw new Error("Id is required");
+    }
+  }
+
+  static generate() {
+    return new Id(Bun.randomUUIDv7());
+  }
+
+  equals(other: this): boolean {
+    return this.value === other.value;
+  }
+}
+
+const item = new Item(
+  Id.generate(),
+  "Bola de basquete",
+  new Price(10),
+  "0194618d-d752-7db0-b51e-82c0ec8846b0",
+  1,
+);
+
 class Order {
   private _items: Item[] = [];
   public createdAt = new Date();
 
   constructor(
-    readonly id: string,
+    readonly id: Id,
     public customerName: string,
     public deliveryPerson: string,
     private _address: Address,
   ) { }
+
+  addItem(item: Item) {
+    const itemAlreadyExists = this._items.find((i) => i.id === item.id);
+    if (itemAlreadyExists) {
+      throw new Error("Item already exists");
+    }
+
+    this._items.push(item);
+  }
+
+  updateItemName(id: Id, name: string) {
+    const itemIndex = this._items.findIndex((i) => i.id.equals(id));
+    if (itemIndex === -1) {
+      throw new Error("Item not found");
+    }
+
+    this._items[itemIndex].name = name;
+  }
+
+  calculateTotal() {
+    return this._items.reduce(
+      (total, item) => total.add(item.getTotal()),
+      new Price(0),
+    );
+  }
+
+  get items(): ReadonlyArray<Readonly<Item>> {
+    return this._items;
+  }
 }
 
 
-const item = new Item(
-  "1",
-  "Bola de basquete",
-  100,
-  "0194618d-d752-7db0-b51e-82c0ec8846b0",
-  1,
-);
-
 const order = new Order(
-  "1",
+  Id.generate(),
   "Camilla",
   "Lucas",
   new Address("city", "street", "country", new Zip("12345678"), "number"),
